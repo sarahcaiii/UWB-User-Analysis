4bb683d90ff498eb9c17c91c54cea00f9a107792
蔡振廷 <James@saiburiteitekiMacBook-Air-2.local>
2019-11-06 15:38:45 +0800

    washed figs

--- utils.py
+++ utils.py
@@ -9,72 +9,13 @@ def get_data_from_string(data_string):
     '''
     return [float(x) for x in data_string.strip(' ()\n').split(',')]
 
-class PlotFile:
-    def __init__(self, file_name):
-        super(PlotFile, self).__init__()
-        self.data_list = list()
-
-        with open(file_name, 'r') as f:
-            for line in f.readlines():
-                self.data_list.append(get_data_from_string(line))
-
-        self.data_list = np.around(np.asarray(self.data_list), decimals = 3)
-
-    def smooth(self, c):
-        N = 50
-        weights=np.hanning(N)
-        return np.convolve(weights/weights.sum(),c)[N-1:-N+1]
-
-    def draw_x(self, pos):
-        plt.subplot(pos)
-        plt.plot(self.data_list[:, 3])
-        plt.plot(self.smooth(self.data_list[:, 3]))
-
-    def draw_y(self, pos):
-        plt.subplot(pos)
-        plt.plot(self.smooth(self.data_list[:, 4]))
-
-    def draw_route(self, pos):
-        plt.subplot(pos)
-        plt.scatter(self.data_list[:, 3], self.data_list[:, 4], s=2)
-        plt.scatter(self.smooth(self.data_list[:, 3]), self.smooth(self.data_list[:, 4]), s=2)
-
-def draw_test(plot):
-    plot.draw_x(311)
-   #plot.draw_y(312)
-    plot.draw_route(313)
-    plt.show()
-
-def heat_test(plot):
-    from scipy.stats import gaussian_kde
-    x = plot.smooth(plot.data_list[:, 3])
-    y = plot.smooth(plot.data_list[:, 4])
-    xy = np.vstack([x,y])
-    z = gaussian_kde(xy)(xy)
-
-    fig, ax = plt.subplots()
-    ax.scatter(x, y, c=z, s=5, edgecolor='')
-    plt.show()
-
-def animation_test(plot):
-    from matplotlib import animation
-    fig, ax = plt.subplots()
-    line, = ax.scatter(plot.data_list[:,3], plot.data_list[:,4])
-
-    def animate(i):
-        line.set_ydata(plot.data_list[i,4])
-        return line,
-    def init():
-        line.set_ydata(plot.data_list[0, 4])
-        return line,
-
-    ani = animation.FuncAnimation(fig=fig,
-                                  func=animate,
-                                  frames=100,
-                                  init_func=init,
-                                  interval=20,
-                                  blit=True)
-    plt.show()
+def get_data_list_from_file(file_name):
+    data_list = list()
+    with open(file_name, 'r') as f:
+        for line in f.readlines():
+            if line != '\n':
+                data_list.append(get_data_from_string(line))
+    return data_list
 
 class TrackPlot:
     def __init__(self, data_list):
@@ -85,15 +26,61 @@ class TrackPlot:
 
         self.data_list = np.asarray(self.data_list)
 
-    def smooth(self, c):
-        N = 50
+    def extreme_filter(self, speed=False):
+        index = list()
+        for (i, data) in enumerate(self.data_list):
+            if data[5] >= 0 and data[5] < 2.5 \
+            and data[3] > -10 and data[3] < 25 \
+            and data[4] > -10 and data[4] < 12:
+                if not speed or (abs(data[6]) < 4 and abs(data[7]) < 4):
+                    index.append(i)
+
+        self.data_list = self.data_list[index]
+
+    def outlier_filter(self, range_list, sigma=2):
+        for k in range_list:
+            data_list = self.data_list[:,k]
+
+            smooth_n = 17
+            smooth_list = self.smooth(data_list, smooth_n)[smooth_n//2:-smooth_n//2-1]
+            #smooth_list = self.smooth(data_list, smooth_n)[smooth_n:-smooth_n]
+            res_list = smooth_list - data_list[smooth_n:-smooth_n]
+            res_mean = np.mean(res_list)
+            res_std = np.std(res_list, ddof=1)
+
+            index = list()
+            for i in range(len(res_list)):
+                if res_list[i] < res_mean - sigma * res_std or res_list[i] > res_mean + sigma * res_std:
+                    continue
+                index.append(i)
+
+            self.data_list = np.append(np.append(self.data_list[:smooth_n], self.data_list[index], axis=0), self.data_list[-smooth_n:-1], axis=0)
+
+    def preprocess(self, test=False):
+        #plt.scatter(self.data_list[:,3], self.data_list[:,4], s=2, label='1')
+        self.extreme_filter()
+        if test:
+            plt.scatter(self.data_list[:,3], self.data_list[:,4], s=2, label='2')
+        self.extreme_filter(True)
+        if test:
+            plt.scatter(self.data_list[:,3], self.data_list[:,4], s=2, label='3')
+        self.outlier_filter(range(3, 9))
+        if test:
+            plt.scatter(self.data_list[:,3], self.data_list[:,4], s=2, label='4')
+            plt.legend()
+
+    def interp(self, c, N=51):
+        from scipy.signal import savgol_filter
+        return savgol_filter(c, N, 2, mode='nearest')
+
+    def smooth(self, c, N=17):
         weights=np.hanning(N)
         return np.convolve(weights/weights.sum(),c)[N-1:-N+1]
 
     def draw_route(self):
         from scipy.stats import gaussian_kde
-        x = self.smooth(self.data_list[:, 3])
-        y = self.smooth(self.data_list[:, 4])
+        x = self.interp(self.data_list[:, 3])
+        y = self.interp(self.data_list[:, 4])
         xy = np.vstack([x,y])
         z = gaussian_kde(xy)(xy)
 
@@ -102,23 +89,29 @@ class TrackPlot:
         plt.ylim((-10, 10))
 
         bg = plt.imread('tmall.png')
-        ax.imshow(bg, extent=[-33, 38, -37, 40])
+        ax.imshow(bg, extent=[-31, 38, -37, 43])
         ax.scatter(x, y, c=z, s=3)
 
-        plt.show()
-
-def main():
-    file_name = './results/0001.txt'
-
-    buffer = list()
-    with open(file_name) as f:
-        for line in f.readlines():
-            buffer.append(get_data_from_string(line))
-    plot = TrackPlot(buffer)
+def draw_test():
+    file_name = './results/0120.txt'
+    data_list = get_data_list_from_file(file_name)
+    plot = TrackPlot(data_list)
+    plot.preprocess()
     plot.draw_route()
-    #heat_test(plot)
-    #animation_test(plot)
+    plt.show()
+
+def save_test():
+    for i in range(342, 401):
+        file_name = './results/%04d.txt' % i
+        data_list = get_data_list_from_file(file_name)
+        plot = TrackPlot(data_list)
+        #plot.preprocess()
+        plot.draw_route()
+        plt.savefig('./raw_figs/%04d.png' % i)
+        plt.close()
+        print('%04d.png' % i)
 
 
 if __name__ == '__main__':
-    main()
+    #draw_test()
+    save_test()
